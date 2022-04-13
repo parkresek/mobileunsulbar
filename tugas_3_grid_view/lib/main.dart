7b3395ae36736fd69bfa12d1a5663e01fcab6088
parkresek <antowiranto47@gmail.com>
2022-04-14 03:07:31 +0800

    grid view

---
+++ tugas_3_grid_view/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BelajarGridView());
+
+class BelajarGridView extends StatelessWidget {
+  final List alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Tugas 3"),
+          ),
+          body: GridView(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
+            children: <Widget>[
+              GridView.count(
+                crossAxisCount: 3,
+                scrollDirection: Axis.horizontal,
+                padding: EdgeInsets.all(20),
+                children: List.generate(alphabet.length, (index) {
+                  return Container(
+                    height: 100,
+                    child: Card(
+                      child: Center(
+                          child: Text(alphabet[index],
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 20))),
+                      color: Color.fromARGB(255, 255, 77, 77),
+                    ),
+                  );
+                }),
+              ),
+              FlutterLogo(),
+              FlutterLogo(),
+              FlutterLogo(),
+              FlutterLogo(),
+            ],
+          )),
+    );
+  }
+}
